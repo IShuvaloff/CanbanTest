ce1a2ebd60f04384907c369e1914e948af7efd55
Ilya A. Shuvalov <i.a.shuvalov@gmail.com>
2023-10-11 00:06:56 +0300

    скорректирован url приложения для деплоя в gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/CanbanTest/' : '/',
 
   chainWebpack: (config) => {
     config.module.rule('svg-sprite').use('svgo-loader').loader('svgo-loader');
